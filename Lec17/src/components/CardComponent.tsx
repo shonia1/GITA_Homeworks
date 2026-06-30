87d46050c24ef00cea1536da0717dbb95f2f36f9
shonia1 <dito23.shonia@gmail.com>
2026-06-30 21:32:18 +0400

    style: improve formatting and add colons for clarity in user information display

--- Lec17/src/components/CardComponent.tsx
+++ Lec17/src/components/CardComponent.tsx
@@ -1,28 +1,31 @@
 import type { IUser } from "../interfaces/IUser";
 
+// ეს არის CardComponent-ის props, სადაც გადმოგვცემენ მომხმარებლების სიას
 interface CardComponentsProps {
   users: IUser[];
 }
-export default function CardComponent({ users }:CardComponentsProps) {
+
+export default function CardComponent({ users }: CardComponentsProps) {
   return (
     <div className="cards-container">
+      {/* თითოეული მომხმარებლისთვის ვქმნით ბარათს და ვაჩვენებთ მის ძირითად ინფორმაციას */}
       {users.map((user: IUser) => (
         <ul className="user-card" key={user.id}>
           <li>
-            <strong>Name</strong> {user.name}
+            <strong>Name:</strong> {user.name}
           </li>
           <li>
-            <strong>Username</strong> {user.username}
+            <strong>Username:</strong> {user.username}
           </li>
           <li>
-            <strong>Email</strong>{" "}
+            <strong>Email:</strong>{" "}
             <a href={`mailto:${user.email}`}>{user.email}</a>
           </li>
           <li>
-            <strong>Phone</strong> {user.phone}
+            <strong>Phone:</strong> {user.phone}
           </li>
           <li>
-            <strong>Website</strong>{" "}
+            <strong>Website:</strong>{" "}
             <a
               href={`https://${user.website}`}
               target="_blank"
@@ -32,7 +35,7 @@ export default function CardComponent({ users }:CardComponentsProps) {
             </a>
           </li>
           <li>
-            <strong>Company Name</strong> {user.company.name}
+            <strong>Company Name:</strong> {user.company.name}
           </li>
         </ul>
       ))}
