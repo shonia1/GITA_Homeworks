a442930df2ffe23f6140c87b85792fb54546c908
shonia1 <dito23.shonia@gmail.com>
2026-07-01 20:48:01 +0400

    feat: integrate Filter component for enhanced product filtering

--- Lec17.1/src/App.tsx
+++ Lec17.1/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import type { IProduct } from "./IProduct";
 import "./App.css";
+import Filter from "./components/filter";
 
 // მთავარი კომპონენტი, რომელიც პროდუქციის სიას აჩვენებს
 export default function App() {
@@ -13,7 +14,9 @@ export default function App() {
     document.title = "Product Catalog";
     const fetchProducts = async () => {
       // API-დან ვიღებთ პროდუქციის სიას
-      const response = await axios.get("https://fakestoreapi.com/products");
+      const response = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products",
+      );
       // მიღებულ მონაცემებს ვუშვებთ state-ში
       setProductData(response.data);
     };
@@ -36,24 +39,18 @@ export default function App() {
 
   return (
     <div>
-      {/* ტექსტური ველი პროდუქციის ძებნისთვის */}
-      <input
-        type="text"
-        value={filterBar}
-        onChange={(e) => setFilterBar(e.target.value)}
+      <Filter
+        filterBar={filterBar}
+        setFilterBar={setFilterBar}
+        filterCategory={filterCategory}
+        setFilterCategory={setFilterCategory}
       />
-      {/* კატეგორიის არჩევის სელექტორი */}
-      <select
-        value={filterCategory}
-        onChange={(e) => setFilterCategory(e.target.value)}
-      >
-        <option value="">ყველა</option>
-        <option value="electronics">ელექტრონიკა</option>
-        <option value="jewelery">იუველირული ნაწარმი</option>
-      </select>
+
       {/* ფილტრირებული პროდუქციების ჩვენება */}
       {filteredProducts.map((product) => (
-        <li key={product.id}>{product.title} - {product.price}$</li>
+        <li key={product.id}>
+          {product.title} - {product.price}$
+        </li>
       ))}
     </div>
   );
